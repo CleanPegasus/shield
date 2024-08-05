e7dcf20f61003ed7d153038225379f834d60fb53
CleanPegasus <arunk609@gmail.com>
2024-08-05 11:17:33 +0530

    🐛 fix verifier

--- contracts/shield.sol
+++ contracts/shield.sol
@@ -2,18 +2,19 @@ pragma solidity ^0.8.19;
 
 import "./verifier.sol";
 
-contract Shield is Groth16Verifier {
+contract Shield {
 
   uint256[5] publicHashes;
-
-  constructor(uint256[5] memory _publicHashes) {
+  Groth16Verifier verifier;
+  constructor(uint256[5] memory _publicHashes, address _verifierAddress) {
     publicHashes = _publicHashes;
+    verifier = Groth16Verifier(_verifierAddress);
   }
 
 
   function verify(uint[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[5] calldata _pubSignals) external view returns (bool isValid) {
     bool isHashEqual = isArrayEqual(_pubSignals);
-    bool proofVerification = verifyProof(_pA, _pB, _pC, _pubSignals);
+    bool proofVerification = verifier.verifyProof(_pA, _pB, _pC, _pubSignals);
   
     isValid = isHashEqual && proofVerification;
   } 
